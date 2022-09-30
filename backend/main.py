fb5608345bdb09db10f20bc17fd91ad018166cfa
joshfigueroa <jrfigueroa1228@gmail.com>
2022-09-30 16:12:07 -0400

    Added some basic HTTP methods for rentals, changed app name in package-lock

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,47 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
+# Instantiate FastAPI app
 app = FastAPI()
 
+# Permit origin from React to allow for connection
 origins = [
-    'http://localhost:3000'
+    'https://localhost:3000'
 ]
 
-app.add.middleware(
+# Define middleware rules for app, currently all methods
+# and headers are allowed for dev purposes 
+# (*SHOULD BE CHANGED IN FINAL VERSION)
+app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['GET'],
-    allow_headers=['Content-Type', 'application/xml']
+    allow_methods=['*'],
+    allow_headers=['*']
 )
 
-
+# Root HTTP method
 @app.get("/")
-async def root():
-    return {"message": "Rent4Hire"}
+def read_root():
+    return {"Rent4Hire"}
+
+# Rental HTTP methods
+@app.get("/api/rental")
+async def get_rental():
+    return 1
+
+@app.get("/api/rental{id}")
+async def get_rental_by_id(id):
+    return 1
+
+@app.post("/api/rental")
+async def post_rental(rental):
+    return 1
+
+@app.put("/api/rental{id}")
+async def put_rental(id, data):
+    return 1
+
+@app.delete("/api/rental{id}")
+async def delete_rental(id):
+    return 1
