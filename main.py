cb889ee2e91ad11ebd2aca418356cde625592ff4
excitedpup <daniellewhitmarsh3@gmail.com>
2022-10-30 11:32:49 -0400

    rm F/Bend folders / db startup fix

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)#TAKE OUT DEBUG BEFORE PRODUCTION
+
+# HAVING ISSUES STARTING WITH EXISTING DATABASE
--- website/__init__.py
+++ website/__init__.py
@@ -11,6 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False      # HOPING TO FIX issue with db at start up
     db.init_app(app)
 
     from .views import views
