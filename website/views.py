f39222b84705af4ae61f26221e47e3df66e9f9f6
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 09:22:07 -0400

    merged pial's changes

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
 from .models import Note
 from . import db
 import json
@@ -64,6 +65,44 @@ def create_rental():
         if request.form.get('chat_button') == "Chat":
             return render_template('chat.html', user=current_user)
     return render_template("create_rental.html", user=current_user)
+
+@views.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile_page():
+    checkAuth = False
+    user = current_user
+    if request.method == 'POST':
+        if checkAuth:
+            print("here")
+            if request.form['submit'] == 'submitNewInfo':
+                userDB = User.query.filter_by(email = user.email).first()
+                userDB.first_name = request.form.get('firstName')
+                userDB.last_name = request.form.get('lastName')
+                userDB.email = request.form.get('email')
+                userDB.street = request.form.get('street')
+                db.session.commit()
+                flash("Profile Updated!", category='success')
+            elif request.form['submit'] == 'Submit New Password':
+                userDB = User.query.filter_by(email = user.email).first()
+                new_password = request.form.get('password')
+                conf_password = request.form.get('confPassword')
+                if new_password == conf_password:
+                    userDB.password = generate_password_hash(new_password, method='sha256')
+                    db.session.commit()
+                    flash("Password Updated!", category='success')
+                else:
+                    flash("Passwords don't match.", category='error')
+        print("Here 2")
+        checkPassword = request.form.get('password')
+        if check_password_hash(user.password, checkPassword):
+            checkAuth = True
+        else:
+            checkAuth = False
+            flash("Incorrect credentials, try again.", category='error')
+        return render_template("profile_dash.html", user = user, checkAuth = checkAuth)
+    elif request.method == 'GET':
+        return render_template("profile_dash.html", user = user, checkAuth = checkAuth)
+
 
 # Route to a testing page, test whatever html/css you want to play with
 @views.route('/test')
